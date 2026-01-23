a2845ec4c39bfc808c0e4bcd6f9ee639b502c9f4
tsuki8neko <dsamurd@gmail.com>
2026-01-22 20:23:43 -0800

    fix: add a title to one-chapter fanfics

--- src/core/getTitle.js
+++ src/core/getTitle.js
@@ -1,3 +1,8 @@
+js
 export function getTitle() {
-    return document.querySelector("h1.heading[itemprop='name']")?.innerText.trim() || "Фанфик";
+    return (
+        document.querySelector("h1.heading[itemprop='name']")?.innerText.trim() ||
+        document.querySelector("h1.heading[itemprop='headline']")?.innerText.trim() ||
+        "Фанфик"
+    );
 }
